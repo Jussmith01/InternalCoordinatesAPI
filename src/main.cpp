c1a0c8be97f215b2b513cd3bc06a4429c519d55a
Jujuman-Home <jussmith48@gmail.com>
2015-10-08 16:57:40 -0400

    Initial

---
+++ src/main.cpp
@@ -0,0 +1,22 @@
+// Standary Library
+#include <stdlib.h>
+#include <stdio.h>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <fstream>
+#include <iomanip>
+#include <vector>
+#include <omp.h>
+#include <regex>
+#include <unordered_map>
+
+// Handlers
+#include "handlers/internalcoordinate.h"
+
+
+int main(int argc, char *argv[]) {
+
+
+    return 0;
+};
