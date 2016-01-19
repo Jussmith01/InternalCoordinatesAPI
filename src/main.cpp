5452da4d2a53353327fe5851da51dd29543e6017
Jujuman-Home <jussmith48@gmail.com>
2016-01-19 18:15:17 -0500

    Added RandomCartesian

--- src/main.cpp
+++ src/main.cpp
@@ -15,65 +15,18 @@
 
 int main(int argc, char *argv[]) {
     using namespace itrnl;
-    std::vector< glm::ivec2 > bndidx =
-        {glm::ivec2(0,1)
-        ,glm::ivec2(0,2)
-        ,glm::ivec2(0,4)
-        ,glm::ivec2(2,5)
-        ,glm::ivec2(3,6)
-        ,glm::ivec2(3,7)
-        ,glm::ivec2(3,4)
-        ,glm::ivec2(4,8)
-        ,glm::ivec2(4,9)};
 
-    std::vector<glm::vec3> xyz =
-        {glm::vec3(0.0000,0.7671,0.0000)
-        ,glm::vec3(0.0000	-0.7671	0.0000)
-        ,glm::vec3(-1.4078	1.3718	0.0000)
-        ,glm::vec3()
-        ,glm::vec3()
-        ,glm::vec3()
-        ,glm::vec3()
-        ,glm::vec3()
-        ,glm::vec3()
-        ,glm::vec3()
-        ,glm::vec3()
-        ,glm::vec3()
-        ,glm::vec3()
-        ,glm::vec3()};
+    std::vector<std::string> xyz =
+        {std::string("C  C1  1.0000 2.0000 3.0000 0.5000")
+        ,std::string("H  H1  1.0000 0.0000 0.0000 0.5000")
+        ,std::string("H  H1  0.0000 2.0000 3.0000 0.25000")
+        ,std::string("H  H2  0.0000 1.0000 2.0000 0.25000")
+        ,std::string("He He2 0.0000 1.0000 0.0000 0.2000")};
 
-    std::vector<std::string> type({
-                             "C" /*1*/
-                            ,"C" /*2*/
-                            ,"C" /*3*/
-                            ,"C" /*4*/
-                            ,"H" /*5*/
-                            ,"H" /*6*/
-                            ,"H" /*7*/
-                            ,"H" /*8*/
-                            ,"H" /*9*/
-                            ,"H" /*10*/
-                            ,"H" /*11*/
-                            ,"H" /*12*/
-                            ,"H" /*13*/
-                            ,"H" /*14*/ });
 
-    Internalcoordinates icrds(bndidx,xyz,type);
+    RandomCartesian rcrds(xyz);
 
-    //icrds.getRandRng().setRandomRanges(rrng);
 
-    icrds.printdata();
-
-    std::string zmat;
-    iCoordToZMat(icrds.getiCoords(),zmat);
-    std::cout << zmat << std::endl;
-
-    std::vector<glm::vec3> xyzf;
-    iCoordToXYZ(icrds.getiCoords(),xyzf);
-    std::cout << "COORDINATES:" << std::endl;
-    for (auto && i : xyzf) {
-        std::cout << "[" << i.x << "," << i.y << "," << i.z << "]" << std::endl;
-    }
 
     //std::vector<int> seeds = {822650048,8938205,51381752,90742112};
     //RandomReal reng(seeds,0.0f,1.0f,std::string("uniform"));
